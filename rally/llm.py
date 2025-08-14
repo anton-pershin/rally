73d676cd87b452a9633ffcc8003102f305ab3f4a
Anton Pershin <tony.pershin@gmail.com>
2025-08-14 05:45:38 +0300

    Added llm family to the llm definition

--- rally/llm.py
+++ rally/llm.py
@@ -5,18 +5,21 @@ class Llm:
     def __init__(
         self,
         url: str,
+        model_family: Optional[str] = None,
         authorization: Optional[str] = None,
         model: Optional[str] = None,
     ) -> None:
         self.url: str = url
+        self.model_family: Optional[str] = model_family
         self.authorization: Optional[str] = authorization
         self.model: Optional[str] = model
 
 
 class LocalLlm(Llm):
-    def __init__(self, url: str) -> None:
+    def __init__(self, url: str, model_family: str) -> None:
         super().__init__(
             url=url,
+            model_family=model_family,
         )
 
 
