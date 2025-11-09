d1c2427d4ea9f9d1014a0c87c260b4e99d074960
Anton Pershin <tony.pershin@gmail.com>
2025-11-09 07:30:05 +0300

    Added to_boolean function

--- rally/utils/common.py
+++ rally/utils/common.py
@@ -8,3 +8,13 @@ def get_project_path() -> Path:
 def get_config_path() -> Path:
     return get_project_path() / "config"
 
+
+def to_boolean(llm_output: str) -> bool:
+    s = llm_output.lower()
+    if s.startswith("yes") or s.startswith("true"):
+        return True
+    elif s.startswith("no") or s.startswith("false"):
+        return False
+
+    raise ValueError(f"LLM output is inconsistent with the boolean type: '{s}'")
+
